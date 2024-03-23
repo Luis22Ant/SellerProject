a4153dd9c643c63e53e7ad4b928d9b7e25281318
Luis Antonio <luisengenhariaeletronica@gmail.com>
2024-03-23 18:23:09 -0300

    Criação de exceção foreign key

---
+++ SalesWebApp/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace SalesWebApp.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+    }
+}
--- SalesWebApp/Services/SellerService.cs
+++ SalesWebApp/Services/SellerService.cs
@@ -36,9 +36,17 @@ namespace SalesWebApp.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("You can't delete because this seller has sales");
+            }
+
         }
 
         public async Task UpdateAsync(Seller obj)
